eeb37e40bff731115c871b8b0ecbcb461bb5fb5c
Korti <lucas.korthauer@gmail.com>
2020-08-25 19:14:50 +0200

    Added close action to the close button. Added open fullscreen and close fullscreen action to the fullscreen button.

--- src/components/TerminalHeader.vue
+++ src/components/TerminalHeader.vue
@@ -5,9 +5,12 @@
                 <b-row class="text-center nav-row">
                     <b-col cols="1">
                         <b-button-group>
-                                <b-button class="round" size="sm" variant="danger"><b-icon-x/></b-button>
+                                <b-button class="round" size="sm" variant="danger" v-on:click="close()"><b-icon-x/></b-button>
                                 <b-button class="round" size="sm" variant="warning"><b-icon-dash/></b-button>
-                                <b-button class="round" size="sm" variant="success"><b-icon-arrows-angle-expand/></b-button>
+                                <b-button class="round" size="sm" variant="success" v-on:click="fullscreen()">
+                                    <b-icon-arrows-angle-expand v-show="!isFullscreen"/>
+                                    <b-icon-arrows-angle-contract v-show="isFullscreen"/>
+                                </b-button>
                             </b-button-group> 
                     </b-col>
                     <b-col cols=10>
@@ -21,7 +24,42 @@
 
 <script>
 export default {
-    
+    data: function() {
+        return {
+            isFullscreen: document.fullscreenElement !== null
+        }
+    },
+    methods: {
+        close: function() {
+            window.close();
+        },
+        fullscreen: function() {
+            const elem = document.documentElement;
+            if(!this.isFullscreen) {
+                if (elem.requestFullscreen) {
+                    elem.requestFullscreen();
+                } else if (elem.mozRequestFullScreen) { /* Firefox */
+                    elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                    elem.webkitRequestFullscreen();
+                } else if (elem.msRequestFullscreen) { /* IE/Edge */
+                    elem.msRequestFullscreen();
+                }
+                this.isFullscreen = true;
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) { /* Firefox */
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) { /* IE/Edge */
+                    document.msExitFullscreen();
+                }
+                this.isFullscreen = false;
+            }
+        }
+    }
 }
 </script>
 
